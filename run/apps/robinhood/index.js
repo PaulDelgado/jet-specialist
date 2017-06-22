427e8b7846eaaf31ea9605eca1a6ed01263383f1
System <none>
2017-06-22 03:31:44 +0000

    #7 TODO-007 Adding proper SessionEndedRequest handling

--- run/apps/robinhood/index.js
+++ run/apps/robinhood/index.js
@@ -10,22 +10,25 @@ app.launch(function(req, res) {
   res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
+// Check for an exit or cancel request
+app.cancel(function(req, res, type) {
+  if (type === 'SessionEndedRequest') {
+    res.send('').shouldEndSession(true);
+    return true;
+  }
+});
+
 app.intent('quoteInfo', {
   'slots': {
     'SYMBOL': 'SYMBOLS'
   },
   'utterances': ['{quote} {|for|on} {|ticker|symbol} {-|SYMBOL}']
 },
-  function(req, res, type) {
+  function(req, res) {
     // Get the slot
     var symbol = req.slot('SYMBOL');
     // Set a reprompt value
     var reprompt = 'Tell me the symbol of a tradeable instrument to get quote data.';
-    // Check for an exit or cancel request
-    if (type === 'SessionEndedRequest') {
-      res.send('').shouldEndSession(true);
-      return true;
-    }
     if (_.isEmpty(symbol)) {
       var prompt = 'I didn\'t hear a ticker symbol. Tell me which ticker symbol you\'d like me to quote for you.';
       res.say(prompt).reprompt(reprompt).shouldEndSession(false);
